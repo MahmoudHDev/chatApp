983e798861308c3ef52be4e989a5d803f72e3544
Mahmoud Hashim <Mahmoud.ios.dev@gmail.com>
2022-06-27 16:36:01 +0200

    adding .gitignore

---
+++ ChatApp/Users/View/UsersViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UsersViewController.swift
+//  ChatApp
+//
+//  Created by Mahmoud on 6/19/22.
+//
+
+import UIKit
+
+class UsersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
